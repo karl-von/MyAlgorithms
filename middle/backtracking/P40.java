9e17fb09207918dc294a2a8c2fd7600b61282c69
karl-von <karlvon514@gmail.com>
2023-10-04 14:13:13 +0900

    add question content into file

--- middle/backtracking/P40.java
+++ middle/backtracking/P40.java
@@ -1,7 +1,52 @@
+package backtracking;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+
+//Given a collection of candidate numbers (candidates) and a target number (
+//target), find all unique combinations in candidates where the candidate numbers sum
+//to target.
+//
+// Each number in candidates may only be used once in the combination.
+//
+// Note: The solution set must not contain duplicate combinations.
+//
+//
+// Example 1:
+//
+//
+//Input: candidates = [10,1,2,7,6,1,5], target = 8
+//Output:
+//[
+//[1,1,6],
+//[1,2,5],
+//[1,7],
+//[2,6]
+//]
+//
+//
+// Example 2:
+//
+//
+//Input: candidates = [2,5,2,1,2], target = 5
+//Output:
+//[
+//[1,2,2],
+//[5]
+//]
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= candidates.length <= 100
+// 1 <= candidates[i] <= 50
+// 1 <= target <= 30
+//
+//
 
 public class P40 {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
