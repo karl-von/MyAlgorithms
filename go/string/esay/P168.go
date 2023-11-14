14781005ee0ba9387953852dc457a1ef37b20137
karl-von <karlvon514@gmail.com>
2023-11-14 16:27:04 +0900

    add an answer

---
+++ go/string/esay/P168.go
@@ -0,0 +1,43 @@
+package esay
+
+// Given an integer columnNumber, return its corresponding column title as it
+// appears in an Excel sheet.
+//
+// For example:
+//
+// A -> 1
+// B -> 2
+// C -> 3
+// ...
+// Z -> 26
+// AA -> 27
+// AB -> 28
+// ...
+//
+// Example 1:
+//
+// Input: columnNumber = 1
+// Output: "A"
+//
+// Example 2:
+//
+// Input: columnNumber = 28
+// Output: "AB"
+//
+// Example 3:
+//
+// Input: columnNumber = 701
+// Output: "ZY"
+//
+// Constraints:
+//
+// 1 <= columnNumber <= 2³¹ - 1
+func convertToTitle(columnNumber int) string {
+	var str string = ""
+	for columnNumber > 0 {
+		columnNumber--
+		str = string('A'+byte(columnNumber%26)) + str
+		columnNumber /= 26
+	}
+	return str
+}
